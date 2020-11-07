69ec0be8213e676d666cd835159cff49c14136e9
IlyaKd <ilyakd@protonmail.com>
2020-11-07 18:05:25 +0300

    added module

--- src/App.js
+++ src/App.js
@@ -1,9 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-
-const text = 'How are you?'
-const number = 12345;
-let flag = true;
+import * as nums from './number';
 
 function App() {
   return (
@@ -11,33 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-        <p style = {{color: 'red'}}>
-          Вывод значения переменной 'text': {text}
-        </p>
-        <p style = {{color: 'red'}}>
-          Вывод числа с переменной 'number': {number}
-        </p>
-        <p style = {{color: 'red'}}>
-          Вывод результата арифметической операции 'number' + 54321 = {number + 54321}
-        </p>
-        <p style = {{color: 'red'}}>
-          Вывод результата логической операции: {flag && 'flag is true'}
-        </p>
-        <p style = {{color: 'red'}}>
-          Использования тернарного условия 'number' > 0: {number > 0 ? 'true' : 'false'}
-        </p>
-        <p style = {{color: 'red'}}>
-          Вывод undefined, null, false и true: {null} {false} {true} {undefined}
+          count * length = {nums.count * nums.length}
         </p>
       </header>
     </div>
