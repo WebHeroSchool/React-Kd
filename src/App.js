06119cd503abd443ffdc408cd6213776f62c01e7
IlyaKd <ilyakd@protonmail.com>
2020-11-09 13:25:14 +0300

    react component

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const ItemList = () => (<div>
+  <h2>
+    Have a nice day!
+  </h2>
+</div>);
+
+const App = () => (<div>
+  <h1>
+    How are things?
+  </h1>
+  <ItemList />
+</div>);
+
+export default App;
