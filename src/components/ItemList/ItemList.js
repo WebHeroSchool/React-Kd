ddc76dcb1e2da3ebea848ccd088378f89b36fc7a
IlyaKd <ilyakd@protonmail.com>
2020-11-14 19:43:51 +0300

    add onClick

--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,9 +3,9 @@ import React from 'react';
 import Item from '../Item/Item';
 import styles from './ItemList.module.css';
 
-const ItemList = ({ items }) => (<ul className={styles.wrap}>
+const ItemList = ({ items, onClickDone }) => (<ul className={styles.wrap}>
   {items.map(item => <li key={item.value}>
-    <Item value={item.value} isDone={item.isDone} />
+    <Item value={item.value} isDone={item.isDone} onClickDone={onClickDone} />
     </li>)}
 </ul>);
 
