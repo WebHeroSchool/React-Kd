c240001fe2c2952a86c1073572112bb660d3c592
IlyaKd <ilyakd@protonmail.com>
2020-11-14 12:34:50 +0300

    component-class

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -4,30 +4,32 @@ import ItemList from '../ItemList/ItemList';
 import Footer from '../Footer/Footer';
 import styles from './App.module.css';
 
-const App = () => {
-  const items = [
-    {
-      value: 'Написать новое приложение',
-      isDone: true
-    },
-    {
-      value: 'Сделать коммит',
-      isDone: false
-    },
-    {
-      value: 'Запушить',
-      isDone: false
-    }
-  ];
-
-  return (
-  <div className={styles.wrap}>
-    <h1 className={styles.title}>Список дел:</h1>
-    <InputItem />
-    <ItemList items={items}/>
-    <Footer count={3} />
-  </div>);
-}
+class App extends React.Component {
+  render() {
+    const items = [
+      {
+        value: 'Написать новое приложение',
+        isDone: true
+      },
+      {
+        value: 'Сделать коммит',
+        isDone: false
+      },
+      {
+        value: 'Запушить',
+        isDone: false
+      }
+    ];
+  
+    return (
+      <div className={styles.wrap}>
+        <h1 className={styles.title}>Список дел:</h1>
+        <InputItem />
+        <ItemList items={items}/>
+        <Footer count={3} />
+      </div>);
+  }
+};
 
 export default App;
 
