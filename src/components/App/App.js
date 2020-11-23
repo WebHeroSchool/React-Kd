12171023e752c302ea1b0bb698a38bf20ef90efb
IlyaKd <ilyakd@protonmail.com>
2020-11-23 22:15:39 +0300

    added return for function App

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -8,24 +8,27 @@ import About from '../About/About';
 import Contacts from '../Contacts/Contacts';
 import styles from './App.module.css';
 
-const App = () => 
-  (<Router>
-    <div className={styles.wrap}>
-      <Card className={styles.sidebar}>
-        <MenuList>
-          <Link to='/' className={styles.link}><MenuItem>Обо мне</MenuItem></Link>
-          <Link to='/todo' className={styles.link}><MenuItem>Дела</MenuItem></Link>
-          <Link to='/contacts' className={styles.link}><MenuItem>Контакты</MenuItem></Link>
-        </MenuList>
-      </Card>
+const App = () => {
+  return (
+    <Router>
+      <div className={styles.wrap}>
+        <Card className={styles.sidebar}>
+          <MenuList>
+            <Link to='/' className={styles.link}><MenuItem>Обо мне</MenuItem></Link>
+            <Link to='/todo' className={styles.link}><MenuItem>Дела</MenuItem></Link>
+            <Link to='/contacts' className={styles.link}><MenuItem>Контакты</MenuItem></Link>
+          </MenuList>
+        </Card>
 
-      <Card className={styles.content}>
-        <Route path='/' exact component={About} />
-        <Route path='/todo' component={Todo} />
-        <Route path='/contacts' component={Contacts} />
-      </Card>
-    </div>
-  </Router>);
+        <Card className={styles.content}>
+          <Route path='/' exact component={About} />
+          <Route path='/todo' component={Todo} />
+          <Route path='/contacts' component={Contacts} />
+        </Card>
+      </div>
+    </Router>
+  );  
+};
 
 export default App;
 
